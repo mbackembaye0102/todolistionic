1ed77edf255fd128a6511b181b6149d6f2bb68e6
mbackembaye0102 <elhadjimbackembaye@gmail.com>
2020-06-28 07:39:40 +0200

    fc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+export const firebaseConfig = {
+  apiKey: "AIzaSyBPVIzxFvwhuKS5cl7FV9yq0WhiHMfNukM",
+  authDomain: "todo-5583d.firebaseapp.com",
+  databaseURL: "https://todo-5583d.firebaseio.com",
+  projectId: "todo-5583d",
+  storageBucket: "todo-5583d.appspot.com",
+  messagingSenderId: "336552095702",
+  appId: "1:336552095702:web:2a038175c1e048c356be77",
+  measurementId: "G-VSLK3NC2YX"
+};
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,
+     IonicModule.forRoot(),
+     AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule],
   providers: [
     StatusBar,
     SplashScreen,
@@ -20,4 +34,8 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+
+
+export class AppModule {
+ 
+}
